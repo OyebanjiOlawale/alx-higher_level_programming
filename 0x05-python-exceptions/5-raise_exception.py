a4dca76f736681a65098eb381f085a25561b43ed
oyebanjiolawale <oyebanji_olawale@yahoo.com>
2021-12-06 13:08:22 -0800

    function that raises a type exception.

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Oyebanji Olawale Amzat <oyebanji_olawale@yahoo.com>
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
