0df7f029e0297623e809f5e2710944afc3b5522d
oyebanjiolawale <oyebanji_olawale@yahoo.com>
2021-12-13 09:54:11 -0800

    an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Oyebanji Olawale Amzat <oyebanji_olawale@yahoo.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
