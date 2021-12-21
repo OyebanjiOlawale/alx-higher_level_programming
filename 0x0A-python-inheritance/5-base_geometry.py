60368a7c368ca3ac2a0d43a4b7b4fd5964009164
oyebanjiolawale <oyebanji_olawale@yahoo.com>
2021-12-21 03:05:20 -0800

    an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Oyebanji Olawale Amzat <oyebanji_olawale@yahoo.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
